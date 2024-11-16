a43c7fe697075d93fbdb57965ac5ee2d89bc8eb5
DESAFIOZUCARITASGAMES2 <zucaritasgames@gmail.com>
2024-11-16 01:56:47 +0000

    Mundo guardado 2024-11-16

--- server.py
+++ server.py
@@ -14,8 +14,9 @@
 # in		      India (Mumbai)
 # jp		      Japón (Tokyo)
 # sa		      Sudamérica (São Paulo)
-# us		      Estados unidos (Ohio)
-# us-cal-1	      Estados unidos (California)
+# us		      Estados u
+#nidos (Ohio)
+# us-cal-1	      Es
 
 
 
